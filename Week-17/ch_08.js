bf95d35448d0a6e493e7182a6a6d22faf8aa9ce5
eslam <eslamayman2372002ii@gmail.com>
2025-04-14 11:50:15 +0200

    comment out redundant calculation in Week 17 exercise

--- Week-17/ch_08.js
+++ Week-17/ch_08.js
@@ -10,4 +10,4 @@ console.log([...n1].reduce((a, b) => a + b) * n2.length);
 // .reduce((a, b) => a + b) adds all elements in the array together (10+30+10+20 = 70)
 // n2.length gives us the number of elements in n2 (which is 3)
 // Finally, we multiply 70 * 3 to get 210
-[...n1].reduce((a, b) => a + b) * n2.length;
+// [...n1].reduce((a, b) => a + b) * n2.length;
