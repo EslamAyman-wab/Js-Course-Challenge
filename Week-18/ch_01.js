473060fe935db6a8342138e7c6a9bc6a637196cc
eslam <eslamayman2372002ii@gmail.com>
2025-04-14 14:16:38 +0200

    test

---
+++ Week-18/ch_01.js
@@ -0,0 +1,6 @@
+let ip = "2001:db8:3333:4444:5555:6666:7777:8888";
+
+//** this is an ipv6 [0-9]  [a-f]    Each character can be [0-9a-fA-F]  */
+
+let ipv6 = /[0-9a-fA-F]{1,4}/;
+console.log(ip.match(ipv6));
