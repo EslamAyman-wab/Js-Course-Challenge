cbd29072cc40fef0d4120b98d43662020637f631
Eslam Ayman <eslamayman2372002ii@gmail.com>
2025-04-28 23:05:07 +0300

    .

---
+++ Week-20/ch_08.2.js
@@ -0,0 +1,10 @@
+import{a , b , c }
+
+
+// main.js File
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
+
+// mod-two.js File
+let a = 10; // Do Not Edit Names
+let b = 20; // Do Not Edit Names
+let c = 30; // Do Not Edit Names
