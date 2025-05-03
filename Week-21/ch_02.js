ee8f94791d0f96dc69436bded482a4669176d2d7
Eslam Ayman <eslamayman2372002ii@gmail.com>
2025-05-03 21:54:14 +0300

    solve ch 1 , 2

---
+++ Week-21/ch_02.js
@@ -0,0 +1,14 @@
+let myRequest = new XMLHttpRequest();
+myRequest.open("GET", "ch_01.json");
+myRequest.send();
+
+//
+
+myRequest.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    console.log(this.responseText);
+    setTimeout(() => {
+      console.log("Data loaded");
+    }, 0);
+  }
+};
